a6d38aad3f0d37dad8fc3357e70e4ff848bc9d26
Harry Miles <harry.miles@aaasat.co.uk>
2026-08-22 13:13:52 +0100

    feat: add profiles and device linking foundation

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,7 +2,7 @@ abstract final class AppConstants {
   static const appName = 'HypeTV';
   static const slogan = 'How You Picture Entertainment';
   static const apiBaseUrl =
-      'https://hypetv.uk';
+      'https://control.hypetv.uk';
   static const activationCodeLength = 5;
   static const githubLatestReleaseUrl =
       'https://api.github.com/repos/harryblackheart/hypetv-android-tv/releases/latest';
